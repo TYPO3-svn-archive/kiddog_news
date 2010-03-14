a48c29092ed4a33b0e7eb64e85db4d1512041dd3
kiddog <kiddog@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-14 22:01:32 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiddog_news/trunk@31099 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/JavaScripts/backend-main.js
+++ Resources/Public/JavaScripts/backend-main.js
@@ -3,7 +3,7 @@ Ext.onReady(function() {
     var tree = new Ext.tree.TreePanel({
         el: 'tree-reorder',
         frame:true,
-        width: 250,
+        width: 320,
         height:400,
         useArrows: true,
         autoScroll: true,
@@ -14,7 +14,7 @@ Ext.onReady(function() {
         dataUrl: '../typo3conf/ext/kiddog_news/Classes/Controller/AjaxController.php',
         root: {
             nodeType: 'async',
-            text: 'News Categorys',
+            text: 'News Categoryies',
             draggable: false,
             id: 'project'
         }
@@ -22,4 +22,76 @@ Ext.onReady(function() {
 
     tree.render();
     tree.getRootNode().expand(true);
+    
+    /**
+     * Edit Category-Node's
+     */
+    tree.on('click', alertID); 
+
+    function alertID(node){
+    	if(tree.getSelectionModel().isSelected(node)){
+    		Ext.Msg.alert('Node-ID', node.id);
+    	}else{
+    		Ext.Msg.alert('else');
+    	}
+    };    
+    
+    /**
+     * Context-Menu
+     */
+	tree.on('click', alertID);
+	tree.on('contextmenu', prepareMenu); 
+
+	var sm = tree.getSelectionModel(); 
+
+	function alertID(node){
+		if(tree.getSelectionModel().isSelected(node)){
+			if (node.isLeaf()){
+				Ext.Msg.alert('Die Node-ID ist ' + node.id + ' leaf: true');
+			} else {
+				Ext.Msg.alert('Die Node-ID ist ' + node.id + ' leaf: false');
+			}
+		}
+	};
+	var Menu = new Ext.menu.Menu({
+		id:'menu',
+		items: [{
+			id:'editNode',
+			handler:editNode,
+			cls:'editNode',
+			text:'edit'
+			},{
+			id:'remove',
+			handler:removeNode,
+			cls:'remove',
+			text: 'delete'
+			}]
+	});
+	function prepareMenu(node, e){
+		node.select();
+		Menu.items.get('remove')[node.attributes.allowDelete ? 'enable' : 'disable']();
+		Menu.showAt(e.getXY());
+	}
+	function editNode(){
+Ext.Msg.alert('Edit','Edit Node')
+	}
+	function expandAll(){
+		Menu.hide();
+		setTimeout(function(){
+			Rroot.eachChild(function(n){
+				n.expand(false, false);
+			});
+		}, 10);
+	}
+	function removeNode(){
+		var n = sm.getSelectedNode();
+		if(n && n.attributes.allowDelete){
+			tree.getSelectionModel().selectPrevious();
+			n.parentNode.removeChild(n);
+		}
+	}
+
+    
+    
+    
 });
