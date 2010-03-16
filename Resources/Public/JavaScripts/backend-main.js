7ad09f45bb1345e856975a16c2d38f3fec1d3412
kiddog <kiddog@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-16 18:19:40 +0000

    JSON-Request implemented
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiddog_news/trunk@31165 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/JavaScripts/backend-main.js
+++ Resources/Public/JavaScripts/backend-main.js
@@ -1,97 +1,95 @@
 Ext.onReady(function() {
-    
-    var tree = new Ext.tree.TreePanel({
-        el: 'tree-reorder',
-        frame:true,
-        width: 320,
-        height:400,
-        useArrows: true,
-        autoScroll: true,
-        animate: true,
-        enableDD: true,
-        containerScroll: true,
-        border: false,
-        dataUrl: '../typo3conf/ext/kiddog_news/Classes/Controller/AjaxController.php',
-        root: {
-            nodeType: 'async',
-            text: 'News Categoryies',
-            draggable: false,
-            id: 'project'
-        }
-    });
+	/**
+	 * Category-Tree
+	 */
+	var tree = new Ext.tree.TreePanel(
+			{
+				el : 'tree-reorder',
+				frame : true,
+				width : 320,
+				height : 400,
+				useArrows : true,
+				autoScroll : true,
+				animate : true,
+				enableDD : true,
+				containerScroll : true,
+				border : false,
+				dataUrl : '../typo3conf/ext/kiddog_news/Classes/Controller/AjaxController.php',
+				root : {
+					nodeType : 'async',
+					text : 'News Categoryies',
+					draggable : false,
+					id : 'getCategoriesByParent'
+				}
+			});
 
-    tree.render();
-    tree.getRootNode().expand(true);
-    
-    /**
-     * Edit Category-Node's
-     */
-    tree.on('click', alertID); 
+	tree.render();
+	tree.getRootNode().expand(true);
 
-    function alertID(node){
-    	if(tree.getSelectionModel().isSelected(node)){
-    		Ext.Msg.alert('Node-ID', node.id);
-    	}else{
-    		Ext.Msg.alert('else');
-    	}
-    };    
-    
-    /**
-     * Context-Menu
-     */
+	/**
+	 * Context-Menu
+	 */
 	tree.on('click', alertID);
-	tree.on('contextmenu', prepareMenu); 
+	tree.on('contextmenu', prepareMenu);
 
-	var sm = tree.getSelectionModel(); 
+	var sm = tree.getSelectionModel();
 
-	function alertID(node){
-		if(tree.getSelectionModel().isSelected(node)){
-			if (node.isLeaf()){
-				Ext.Msg.alert('Die Node-ID ist ' + node.id + ' leaf: true');
-			} else {
-				Ext.Msg.alert('Die Node-ID ist ' + node.id + ' leaf: false');
-			}
-		}
+	function alertID(node) {
+		Ext.Ajax.request({
+	       url : '../typo3conf/ext/kiddog_news/Classes/Controller/AjaxController.php',
+	       method: 'POST',
+	       params :{'node':'getCategoryInfoByUid','category':node.id},
+	       
+	       success: function(response, opts) {
+	    	   var obj = Ext.decode(response.responseText);
+	    	   console.dir(obj);
+	           
+	    	   Ext.Msg.alert('Info', 'response.status: '+ response.status);
+	       },
+	       
+	       failure: function(result, request) {
+	    	   var jsonData = Ext.util.JSON.decode(result.responseText);
+	           var resultMessage = jsonData.data.result;
+	           Ext.Msg.alert('Info', 'failure: '+resultMessage);
+	       }
+	   });
 	};
-	var Menu = new Ext.menu.Menu({
-		id:'menu',
-		items: [{
-			id:'editNode',
-			handler:editNode,
-			cls:'editNode',
-			text:'edit'
-			},{
-			id:'remove',
-			handler:removeNode,
-			cls:'remove',
-			text: 'delete'
-			}]
+	
+	var Menu = new Ext.menu.Menu( {
+		id : 'menu',
+		items : [ {
+			id : 'newNode',
+			handler : newNode,
+			cls : 'newNode',
+			text : 'New category into'
+		}, {
+			id : 'editNode',
+			handler : editNode,
+			cls : 'editNode',
+			text : 'edit'
+		}, {
+			id : 'delete',
+			handler : deleteNode,
+			cls : 'delete',
+			text : 'delete'
+		} ]
 	});
-	function prepareMenu(node, e){
+	
+	function prepareMenu(node, e) {
 		node.select();
-		Menu.items.get('remove')[node.attributes.allowDelete ? 'enable' : 'disable']();
 		Menu.showAt(e.getXY());
 	}
-	function editNode(){
-Ext.Msg.alert('Edit','Edit Node')
+	
+	function newNode() {
+		Ext.Msg.alert('New', 'New Node')
 	}
-	function expandAll(){
-		Menu.hide();
-		setTimeout(function(){
-			Rroot.eachChild(function(n){
-				n.expand(false, false);
-			});
-		}, 10);
-	}
-	function removeNode(){
-		var n = sm.getSelectedNode();
-		if(n && n.attributes.allowDelete){
-			tree.getSelectionModel().selectPrevious();
-			n.parentNode.removeChild(n);
-		}
+	
+	function editNode() {
+		Ext.Msg.alert('Edit', 'Edit Node')
+	}	
+	
+	function deleteNode() {
+		Ext.Msg.alert('Delete', 'Delete Node')
 	}
 
-    
-    
-    
 });
