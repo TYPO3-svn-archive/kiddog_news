572095ef26b298d99e048c9084de8bc23e9dba4f
kiddog <kiddog@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-16 15:50:33 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kiddog_news/trunk@31159 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/AjaxController.php
+++ Classes/Controller/AjaxController.php
@@ -1,22 +1,27 @@
 <?php
 $node = "";
-$out = "";
 
 if (isset($_REQUEST["node"])) {
 	$node = $_REQUEST["node"];
 }
 
 switch ($node) {
-
-	case "project":
-		$out = getCategoriesByParent();
-		break;
+	case "getCategoryInfoByUid":
+			getCategoryInfoByUid($_REQUEST["category"]);
+		break;	
+	case "getCategoriesByParent":
+			getCategoriesByParent();
+		break;	
 	default:
-		$out = getCategoriesByParent($node);
-
+			getCategoriesByParent($node);
 }
-echo utf8_encode($out);
 
+/**
+ * Generate data for tree-view
+ * 
+ * @param unknown_type $node
+ * @return unknown_type
+ */
 function getCategoriesByParent($node=1) {
 	$con = mysql_connect("localhost","root","");
 	if (!$con){
@@ -47,7 +52,35 @@ function getCategoriesByParent($node=1) {
 	 *	]
 	 */
 	
-	return $tree; 
+	echo utf8_encode($tree);
+}
+
+
+/**
+ * Get data of a given category-uid
+ * @return unknown_type
+ */
+function getCategoryInfoByUid($uid) {
+	$con = mysql_connect("localhost","root","");
+	if (!$con){
+  		die('Could not connect: ' . mysql_error());
+  	}
+
+	mysql_select_db("T3debug", $con);
+
+	$result = mysql_query(
+		"SELECT name, uid
+		FROM tx_kiddognews_domain_model_category
+		WHERE uid =".$uid
+	);
+
+	$categoryInfos = '[';
+	while($row = mysql_fetch_array($result)){
+		$categoryInfos .= '{"name":"'.$row['name'].'","id":"'.$row['uid'].'"}';
+  	}
+	$categoryInfos .= ']';
+  	mysql_close($con);	
+	echo utf8_encode($categoryInfos);
 }
 
 ?>
